87580d8d151cf8a4d60cac290a25ed013b6e0eb2
JakiMauJadiHengker <nulgathc@gmail.com>
2024-09-09 22:33:43 +0700

    Selesaikan konflik merge

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Add animation on load
 window.onload = () => {
   document.querySelector('.hero').classList.add('show');
@@ -16,3 +17,23 @@ window.addEventListener('scroll', () => {
       }
   });
 });
+=======
+// Add animation on load
+window.onload = () => {
+  document.querySelector('.hero').classList.add('show');
+  document.querySelector('.about').classList.add('show');
+};
+
+// Scroll animation
+window.addEventListener('scroll', () => {
+  const sections = document.querySelectorAll('section');
+  const triggerPoint = window.innerHeight / 1.2;
+
+  sections.forEach(section => {
+      const top = section.getBoundingClientRect().top;
+      if (top < triggerPoint) {
+          section.classList.add('show');
+      }
+  });
+});
+>>>>>>> 73e8428cf076d7ca32b48f2c41840ee2edccb51d
